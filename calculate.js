78a2de01b66b624016d976545532cc80fd84cb2a
maksymilianMroz <mrozmaksymilian@gmail.com>
2022-01-27 00:15:54 +0100

    Little modification to calculate script

--- calculate.js
+++ calculate.js
@@ -1,21 +1,35 @@
+//
+// Variables
+//
 let context = "";
 let numbers = [];
 let score = "";
 let isInputsValid = true;
-
 const inputs = [...document.querySelectorAll("input")];
 
+//
+// Methods
+//
+/**
+ * Calculate the score
+ * @param  {String, Array}    string as a context
+ *                            array numbers to calculate
+ * @return {Number || String} score or text to show
+ */
 const calculateByContext = (context, numbers) => {
   if (!numbers.length) return;
 
+  // check if number from array is not really a number
   numbers.forEach((number) => {
     if (isNaN(number)) {
       isInputsValid = false;
     }
   });
 
+  // if in array of number is one non-number element return text
   if (!isInputsValid) return "Wrong input data";
 
+  // calculations depends on context
   if (context === "sum") {
     return numbers.reduce((a, b) => +a + +b);
   }
@@ -29,11 +43,17 @@ const calculateByContext = (context, numbers) => {
   }
 };
 
-document.addEventListener("click", (event) => {
+/**
+ * Handle click events
+ * @param  {Event} event The event object
+ */
+const clickHandler = (event) => {
+  // check context
   event.target.classList.contains("square--red") && (context = "sum");
   event.target.classList.contains("square--yellow") && (context = "substract");
   event.target.classList.contains("square--blue") && (context = "multiply");
 
+  // add numbers from inputs to array
   inputs.forEach((input) => {
     if (input.value === "") return;
 
@@ -42,9 +62,14 @@ document.addEventListener("click", (event) => {
 
   score = calculateByContext(context, numbers);
 
+  // show score if its not undefined
   if (score !== undefined) document.querySelector("output").innerHTML = score;
 
+  // variables reset
   numbers = [];
   context = "";
   isInputsValid = true;
-});
+};
+
+// Listen for click events
+document.addEventListener("click", clickHandler, false);
